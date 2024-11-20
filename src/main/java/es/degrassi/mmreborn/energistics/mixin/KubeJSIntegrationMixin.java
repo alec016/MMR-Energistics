be036db2f52ac721f21c71e1735be4e1d6f08b72
alec016 <aaronquemadadiaz@gmail.com>
2024-11-20 10:26:39 +0100

    redo: input bus textures rename: screen textures remove: kubejs integration mixin(not necessary) remove: all gui old textures add: tags refactor: JeiPositionedRequirement to match the newest version of mmr

--- src/main/java/es/degrassi/mmreborn/energistics/mixin/KubeJSIntegrationMixin.java
+++
@@ -1,8 +0,0 @@
-package es.degrassi.mmreborn.energistics.mixin;
-
-import es.degrassi.mmreborn.common.integration.kubejs.MachineRecipeBuilderJS;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin({ MachineRecipeBuilderJS.class })
-public abstract class KubeJSIntegrationMixin {
-}
