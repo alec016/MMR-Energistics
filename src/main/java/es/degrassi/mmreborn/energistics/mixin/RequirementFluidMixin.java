e3bb83a4e4b1e0c038f6488f8b3f905f65292aff
alec016 <aaronquemadadiaz@gmail.com>
2024-11-21 19:08:18 +0100

    fix: duplicate fluid output on ae2 hatches update: project ids and dependencies for publishing

--- src/main/java/es/degrassi/mmreborn/energistics/mixin/RequirementFluidMixin.java
+++ src/main/java/es/degrassi/mmreborn/energistics/mixin/RequirementFluidMixin.java
@@ -10,6 +10,7 @@ import es.degrassi.mmreborn.common.util.HybridTank;
 import es.degrassi.mmreborn.energistics.common.util.AETankHolder;
 import net.minecraft.core.HolderLookup;
 import net.neoforged.neoforge.fluids.FluidStack;
+import net.neoforged.neoforge.fluids.capability.IFluidHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -26,5 +27,16 @@ public abstract class RequirementFluidMixin extends ComponentRequirement<FluidSt
       return holder.getOwner().copyTank();
     }
     return CopyHandlerHelper.copyTank(inventory, pRegistries);
+  }
+
+  @Redirect(at = @At(value = "INVOKE", target = "Les/degrassi/mmreborn/common/util/HybridTank;fill" +
+      "(Lnet/neoforged/neoforge/fluids/FluidStack;" +
+      "Lnet/neoforged/neoforge/fluids/capability/IFluidHandler$FluidAction;)I", ordinal = 1), method =
+      "canStartCrafting")
+  public int fill(HybridTank instance, FluidStack fluidStack, IFluidHandler.FluidAction fluidAction) {
+    if (instance instanceof AETankHolder holder) {
+      return holder.fill(fluidStack, IFluidHandler.FluidAction.SIMULATE);
+    }
+    return instance.fill(fluidStack, fluidAction);
   }
 }
